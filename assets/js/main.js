84ef13fb7c6d9f714194bf63817b85f1665ff8d6
ILu4n <silllvaluannn@gmail.com>
2024-11-21 16:50:48 -0300

    tirando a verbosidade do codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,42 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url)
-    .then((response) => response.json())
-    .then((bodyJson) => {
+function covertPokemonToLi(pokemon){
+    return `
+    
+     <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detalhe">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"alt="${pokemon.name}">
+                </div>
+    
+    `
+}
 
-        debugger
-        console.log(bodyJson)
+const pokemonList = document.getElementById('pokemonList')  
 
+
+    pokeApi.getpokemons().then((pokemons = []) => {
+        const listItens = []
+
+        const newLista = pokemons.map((pokemon) => {
+            return convertPokemonToLi(pokemon)
+        })
+
+        const newHtml = newList.join()
+
+        pokemonList.innerHTML += newHtml
+
+        //const ListItens = {}
+
+     //   for (let i = 0; i < pokemons.length; i++){
+//            const pokemon = pokemons[i];
+  //          listItens.push(convertPokemonToLi(pokemon))
+    //    }
+   //     console.log(listItens)
     })
-    .catch((erro) => console.error(erro));
